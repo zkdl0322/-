907285509ecb4aea53a749bf6d3ae5f3650ddac6
zkdl0322 <106359415+zkdl0322@users.noreply.github.com>
2022-06-10 11:25:22 +0900

    Add files via upload

---
+++ 19장(SwipeGesture)/SwipeGesture/실습 1. ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  PinchGesture
+//
+//  Created by Ho-Jeong Song on 2021/12/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var txtPinch: UILabel!
+    
+    var initialFontSize:CGFloat!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.doPinch(_:)))
+        self.view.addGestureRecognizer(pinch)
+    }
+
+    @objc func doPinch(_ pinch: UIPinchGestureRecognizer) {
+        if (pinch.state == UIGestureRecognizer.State.began) {
+            initialFontSize = txtPinch.font.pointSize
+        } else {
+            txtPinch.font = txtPinch.font.withSize(initialFontSize * pinch.scale)
+        }
+    }
+
+}
+
